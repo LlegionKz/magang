4ac5751198b34b1860913134e43d31eeb495a7aa
Rafi Nur Ardiansyah <rafimaster9@gmail.com>
2025-08-07 14:44:19 +0700

    fix(courses): update course fee for WordPress & Elementor course

--- api/Courses.js
+++ api/Courses.js
@@ -25,7 +25,7 @@ const Courses = [
       author:author1,
       authortitle:'Robert Henry',
       lesson:'20',
-      fee: '1337',
+      fee: '2025',
       student:'200',
       ratting:'4.5',
       title: 'Learn WordPress & Elementor for Beginners',
